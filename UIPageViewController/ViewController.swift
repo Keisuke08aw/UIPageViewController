626b8a2db6faf86527a7a9dd5ee3136b227366e7
Keisuke <keisuke.umezawa@g.softbank.co.jp>
2021-01-12 16:52:52 +0900

    Initial Commit

---
+++ UIPageViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageViewController
+//
+//  Created by Umezawa Keisuke on 2021/01/12.
+//  Copyright © 2021 Umezawa Keisuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
